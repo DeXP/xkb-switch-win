65406a4684fc04672158fe971ed7a84d33ba2099
DeXP <root@dexp.in>
2013-04-02 14:46:47 +0300

    added getLocalizedCharByUS

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
 xkb-switch-win
 ==============
 
-xkb-switch-lib API port to Win32/Win64. Needed for Vim plugin: https://github.com/lyokha/vim-xkbswitch . Implemented only the functions necessary for the plugin! 
+xkb-switch-lib API port to Win32/Win64. Needed for vim-xkbswitch plugin: http://www.vim.org/scripts/script.php?script_id=4503 (GitHub: https://github.com/lyokha/vim-xkbswitch ) .
+
+In windows you need not only the plugin, but DLL-files from /bin folder. If you have 64-bit Vim, you need libxkbswitch64.dll. For 32-bit version use libxkbswitch32.dll. If you do not know - you can get both, plugin detects verion automatically.
+
+By default you need to put DLL-file into top directory of Vim. Or you need to set g:XkbSwitchLib variable:
+let g:XkbSwitchLib = 'c:\path\to\dll\libxkbswitch32.dll'  
+
+
+==============
+
+Author: Dmitry Hrabrov a.k.a. DeXPeriX
+
 
 Original xkb-switch for Linux: https://github.com/ierton/xkb-switch 
 
--- libxkbswitchwin.c
+++ libxkbswitchwin.c
@@ -21,8 +21,12 @@
 
 #define MAX_LAYS 10
 #define LBUF 3
+#define KBUF_SIZE 256
+
 
 char lName[LBUF+1];
+char aName[LBUF+1];
+char keybuf[KBUF_SIZE];
 
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
@@ -91,4 +95,60 @@ const char *  Xkb_Switch_setXkbLayout( const char *  newgrp ){
         }
     }
     return NULL;
+}
+
+
+__declspec(dllexport)
+const char *  Xkb_Switch_getLocalizedCharByUS( char c, const char * grp ){
+    unsigned int i, n, Lid;
+    HKL  lpList[MAX_LAYS];
+    HKL  currentLayout;
+
+    short key, topkey;
+    char keyboardState[KBUF_SIZE];
+    unsigned long wbuf = 0;
+    int canConvert = 0;
+
+    n = GetKeyboardLayoutList(0, NULL);
+    n = GetKeyboardLayoutList(n, lpList);
+
+    //key = VkKeyScan(c);
+    key = VkKeyScanEx(c, 1033); //get key from us-layout
+    topkey = (key & 0xFF00) >> 8;
+
+    keybuf[0] = 0;
+    keybuf[1] = 0;
+    keybuf[2] = 0;
+    for(i=0; i< ( sizeof(keyboardState)/sizeof(keyboardState[0]) ); i++) keyboardState[i] = 0;
+    if( topkey & 1 ) keyboardState[VK_SHIFT] = 0xFF;
+    if( topkey & 2 ) keyboardState[VK_CONTROL] = 0xFF;
+    if( topkey & 4 ) keyboardState[VK_MENU] = 0xFF;
+
+    for(i=0; i<n; i++){
+        Lid = ((unsigned int)lpList[i]) & 0x0000FFFF; /* bottom 16 bit */
+        currentLayout = lpList[i];
+        LCID localez = MAKELCID( (LANGID)Lid, SORT_DEFAULT);
+        GetLocaleInfo(localez, LOCALE_SISO3166CTRYNAME, aName, LBUF);
+        if(
+           ( dtolower(grp[0]) == dtolower(aName[0]) ) &&
+           ( dtolower(grp[1]) == dtolower(aName[1]) )
+        ){
+            //res = MapVirtualKeyEx(key, DMAPVK_VK_TO_CHAR, currentLayout);
+            //canConvert = ToUnicodeEx( key, 0, keyboardState, keybuf, KBUF_SIZE, 0, currentLayout );
+            canConvert = ToAsciiEx( key, 0, keyboardState, &wbuf, 0, currentLayout );
+            if( canConvert > 0 ){
+                keybuf[0] = wbuf;
+                keybuf[canConvert] = 0;
+            }
+        }
+    }
+
+    return keybuf;
+}
+
+
+__declspec(dllexport)
+const char *  Xkb_Switch_getCurrentCharByUS(const char * curChar){
+    char c = curChar[0];
+    return Xkb_Switch_getLocalizedCharByUS( c, Xkb_Switch_getXkbLayout(NULL) );
 }
